8870acefc904ad23ed0dc8819a661c26397c4347
Hemasri Kottapalli <120649018+HemasriKottapalli@users.noreply.github.com>
2023-06-20 03:24:06 -0700

    Update ChangePin.java
    text align

--- atmUsingJdbc/ChangePin.java
+++ atmUsingJdbc/ChangePin.java
@@ -35,7 +35,7 @@ public class ChangePin {
          Class.forName("com.mysql.cj.jdbc.Driver");
  	     Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/atm","root","mysql123");
  	     PreparedStatement ps = con.prepareStatement("update userdata set pinCode=?  where accNo=?");
- 		 ps.setString(1, pin);
+ 	     ps.setString(1, pin);
  	     ps.setString(2,accNo);
  		 int row=ps.executeUpdate();
  		 if(row>0)
