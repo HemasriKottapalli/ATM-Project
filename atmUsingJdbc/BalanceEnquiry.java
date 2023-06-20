8fab38e36fbe7e810bcb5ce5ae9fcedcda93096f
Hemasri Kottapalli <120649018+HemasriKottapalli@users.noreply.github.com>
2023-06-20 03:17:23 -0700

    Update BalanceEnquiry.java
    edited text alignment

--- atmUsingJdbc/BalanceEnquiry.java
+++ atmUsingJdbc/BalanceEnquiry.java
@@ -10,15 +10,16 @@ public class BalanceEnquiry {
 	//get balance
 	static int getBalance(String accNo) throws ClassNotFoundException, SQLException
 	{
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/atm","root","mysql123");
-		Statement s = con.createStatement();
+	    Class.forName("com.mysql.cj.jdbc.Driver");
+	    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/atm","root","mysql123");
+	    Statement s = con.createStatement();
 	    ResultSet rs=s.executeQuery("select * from userdata where accNo=\""+accNo+"\";");
 	    //didnt write condition for if query fails
 	    int balance=0;
-	    while(rs.next()) {
-			balance=rs.getInt(6);
-		  }
+	    while(rs.next()) 
+	    {
+		balance=rs.getInt(6);
+	    }
 		return balance;
 	}
 }
