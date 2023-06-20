62c7dd23eb934f0b101bf9f3d2a6b03c77a4e198
Hemasri Kottapalli <120649018+HemasriKottapalli@users.noreply.github.com>
2023-06-20 03:31:37 -0700

    Update WithdrawMoney.java
    text align

--- atmUsingJdbc/WithdrawMoney.java
+++ atmUsingJdbc/WithdrawMoney.java
@@ -24,12 +24,12 @@ public class WithdrawMoney {
 	 }
 	//update balance
 	 void updateBalance() throws ClassNotFoundException, SQLException{
-		 Class.forName("com.mysql.cj.jdbc.Driver");
+	     Class.forName("com.mysql.cj.jdbc.Driver");
 	     Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/atm","root","mysql123");
 	     PreparedStatement ps = con.prepareStatement("update userdata set balance=?  where accNo=?");
 	     int balance = BalanceEnquiry.getBalance(accNo);
 	     int x= balance-amount;
-		 ps.setInt(1, x);
+	     ps.setInt(1, x);
 	     ps.setString(2,accNo);
 		 int row=ps.executeUpdate();
 		 if(row>0)
