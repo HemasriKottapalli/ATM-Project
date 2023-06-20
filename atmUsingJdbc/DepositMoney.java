ca064f2c965980154f5a0e5950c2fd1d50b0d818
Hemasri Kottapalli <120649018+HemasriKottapalli@users.noreply.github.com>
2023-06-20 03:25:39 -0700

    Update DepositMoney.java
    text align

--- atmUsingJdbc/DepositMoney.java
+++ atmUsingJdbc/DepositMoney.java
@@ -15,20 +15,20 @@ public class DepositMoney {
 	 }
 	//update balance
 	 void updateBalance() throws ClassNotFoundException, SQLException{
-		 Class.forName("com.mysql.cj.jdbc.Driver");
+	     Class.forName("com.mysql.cj.jdbc.Driver");
 	     Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/atm","root","mysql123");
 	     PreparedStatement ps = con.prepareStatement("update userdata set balance=? where accNo=?");
 	     int balance = BalanceEnquiry.getBalance(accNo);
 	     int x= balance+amount;
-		 ps.setInt(1, x);
+	     ps.setInt(1, x);
 	     ps.setString(2,accNo);
-		 int row=ps.executeUpdate();
-		 if(row>0)
-		  {
-		    System.out.println("Transaction Succesfull!");
-		  }else {
-		    System.err.println("Error Occurred");
-		  }
+	     int row=ps.executeUpdate();
+	     if(row>0)
+	     {
+	       System.out.println("Transaction Succesfull!");
+	     }else {
+	       System.err.println("Error Occurred");
+	     }
 
 	 }
 }
